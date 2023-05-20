c9127683b21b5102bbd2cc35e3ffc9c571189d5b
destitutor <binarybard@pm.me>
2023-05-20 15:06:25 +0900

    first commit

---
+++ src/main/java/com/paypilot/paypilot/PaypilotApplication.java
@@ -0,0 +1,13 @@
+package com.paypilot.paypilot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaypilotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaypilotApplication.class, args);
+	}
+
+}
